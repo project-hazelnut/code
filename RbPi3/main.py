bae1faaf8d08dbe9b119ae6a655e4bea1fbc8db3
Guber <emanuel.guberovic@fer.hr>
2019-03-11 13:48:36 +0000

    added evaluations

--- RbPi3/main.py
+++ RbPi3/main.py
@@ -12,38 +12,40 @@ ina = INA219(shunt_ohms=0.1,
              max_expected_amps = 0.2,
              address=0x40)
 
-ina.configure(voltage_range=ina.RANGE_32V,
+ina.configure(voltage_range=ina.RANGE_16V,
               gain=ina.GAIN_AUTO,
               bus_adc=ina.ADC_128SAMP,
               shunt_adc=ina.ADC_128SAMP)
 
 power_measurements = []
 
-# power measurement delay in seconds
-POWER_MEASUREMENT_DELAY = 1
-MEASUREMENT = "test1"
+# power measurement delay in seconds (1, 2, 4, 8, 16, 32, 64, 100)
+MEASUREMENT = "wake_every_80s"
+POWER_MEASUREMENT_DELAY = 0.1
 
 def main():
     start_time = last_iteration_time = time.time()
     elapsed_time = 0
     
-    while elapsed_time < 3600:
-        power_reading = ina.power() / 1000
+    while elapsed_time < 800:
+        power_reading = ina.power()
         
-        print([MEASUREMENT, power_reading])
+        print([MEASUREMENT, power_reading], " elapsed time ", elapsed_time)
+        
+        
         
         try:
             if not os.path.exists("./results/"):
                 os.makedirs("./results/")
             
-            if not os.path.isfile("./results/"+ MEASUREMENT +".csv"):
-                with open("./results/"+ MEASUREMENT +".csv", "w") as results_file:
+            if not os.path.isfile("./results/"+ str(MEASUREMENT) +".csv"):
+                with open("./results/"+ str(MEASUREMENT) +".csv", "w") as results_file:
                     writer = csv.writer(results_file)
                     writer.writerow(["measurement", "average_power"])
                     writer.writerow([MEASUREMENT, power_reading])
                     results_file.close()
             else:
-                with open("./results/"+ MEASUREMENT +".csv", "w") as results_file:
+                with open("./results/"+ str(MEASUREMENT) +".csv", "a") as results_file:
                     writer = csv.writer(results_file)
                     writer.writerow([MEASUREMENT, power_reading])
                     results_file.close()
@@ -57,6 +59,7 @@ def main():
 
 if __name__ == "__main__":
     main()   
+
 
 
 
